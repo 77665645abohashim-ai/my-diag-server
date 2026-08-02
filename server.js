1bf35a5d123b81c68afc3a074ecc7717d6397294
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-08-02 10:58:19 +0300

    Update server.js

--- server.js
+++ server.js
@@ -9,22 +9,24 @@ app.get('/api/v2/urls', (req, res) => {
     msg: "success",
     message: "success",
     data: {
+      config_no: "0",
+      hosts: [
+        "https://my-diag-server.onrender.com"
+      ],
       login: "https://my-diag-server.onrender.com/api/v2/login",
       login_url: "https://my-diag-server.onrender.com/api/v2/login",
       user_login: "https://my-diag-server.onrender.com/api/v2/login",
-      publicsoftservice.nt: "https://my-diag-server.onrender.com/publicsoftservice.nt",
-      publicsoftservice_nt: "https://my-diag-server.onrender.com/publicsoftservice.nt",
       product_service: "https://my-diag-server.onrender.com/api/v2/product-service",
       getShopRemindStatus: "https://my-diag-server.onrender.com/api/v2/getShopRemindStatus",
       urls: [
-        { key: "login", url: "https://my-diag-server.onrender.com/api/v2/login" },
-        { key: "login_url", url: "https://my-diag-server.onrender.com/api/v2/login" },
-        { key: "publicsoftservice.nt", url: "https://my-diag-server.onrender.com/publicsoftservice.nt" },
-        { key: "publicsoftservice_nt", url: "https://my-diag-server.onrender.com/publicsoftservice.nt" },
-        { key: "product_service", url: "https://my-diag-server.onrender.com/api/v2/product-service" }
-      ],
-      hosts: [
-        "https://my-diag-server.onrender.com"
+        {
+          key: "login",
+          url: "https://my-diag-server.onrender.com/api/v2/login"
+        },
+        {
+          key: "product_service",
+          url: "https://my-diag-server.onrender.com/api/v2/product-service"
+        }
       ],
       config: {
         check_ver: "0",
