0b1514367330fd075046cc98aeeb870f08b86cca
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-08-06 15:09:55 +0300

    Update server.js

--- server.js
+++ server.js
@@ -4,73 +4,167 @@ const path = require('path');
 
 const app = express();
 
-// ----------------------------------------------------
-// 1. الإعدادات والوسائط (Middlewares)
-// ----------------------------------------------------
 app.use(cors());
 app.use(express.json());
 app.use(express.text({ type: '*/*' }));
 app.use(express.urlencoded({ extended: true }));
 
-// مجلد الملفات العامة لتنزيل ملفات الـ ZIP والـ APK
 app.use('/files', express.static(path.join(__dirname, 'public/files')));
 app.use('/api/v2/download', express.static(path.join(__dirname, 'public/files')));
 
 const MY_SERVER_URL = process.env.SERVER_URL || "https://my-diag-server.onrender.com";
 
-// طباعة الطلبات الواردة للمتابعة
 app.use((req, res, next) => {
     console.log(`[${new Date().toISOString()}] ${req.method} -> ${req.url}`);
     next();
 });
 
-// ----------------------------------------------------
-// 2. مسار البرامج العامة (Firmware + التطبيق الأساسي)
-// ----------------------------------------------------
+// المسار الرئيسي الشامل الذي يستجيب لـ queryLatestPublicSofts وكل مسارات الـ Softwares
 app.all([
     '/api/v2/publicsoftservice-nt', 
     '/publicsoftservice-nt', 
     '/publicsoftservice',
-    '/api/v2/publicsoftservice'
+    '/api/v2/publicsoftservice',
+    '/api/v2/diagsoftservice',
+    '/diagsoftservice',
+    '/api/v2/x431paddiagsoftservice',
+    '/x431paddiagsoftservice'
 ], (req, res) => {
     res.setHeader('Content-Type', 'text/xml; charset=utf-8');
-    console.log("✅ [PUBLICSOFT]: Serving Firmware & App");
+    console.log("✅ [MASTER SERVICE]: Responding with queryLatestPublicSofts including all brands");
 
     const xmlResponse = `<?xml version="1.0" encoding="UTF-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://diagzone.com">
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://diagzone.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
     <SOAP-ENV:Body>
         <ns1:queryLatestPublicSoftsResponse>
             <return>
                 <code>0</code>
                 <message>success</message>
                 <x431PadSoftList>
+                    <!-- 1. Diagzone PRO V2 -->
                     <x431PadSoft>
-                        <softId>873</softId>
-                        <softName>Firmware</softName>
-                        <softPkgName>Firmware</softPkgName>
-                        <softPackageID>DOWNLOAD</softPackageID>
-                        <versionNo>V11.91</versionNo>
-                        <versionDetailId>343730</versionDetailId>
-                        <fileSize>393300</fileSize>
-                        <softType>0</softType>
+                        <fileSize>68365802</fileSize>
                         <lanId>EN</lanId>
                         <serverCurrentTime>2026-08-06</serverCurrentTime>
-                        <softUpdateTime>2023-03-27 00:00:00</softUpdateTime>
-                        <url>${MY_SERVER_URL}/files/Firmware_V11.91.zip</url>
-                    </x431PadSoft>
-                    <x431PadSoft>
                         <softId>1015</softId>
                         <softName>Diagzone PRO V2</softName>
-                        <softPkgName>Diagzone PRO V2</softPkgName>
                         <softPackageID>Diagzone_PRO_V2</softPackageID>
-                        <versionNo>V2.00.033</versionNo>
+                        <softUpdateTime>2025-03-08 00:00:00</softUpdateTime>
                         <versionDetailId>359645</versionDetailId>
-                        <fileSize>68365802</fileSize>
-                        <softType>0</softType>
+                        <versionNo>V2.00.033</versionNo>
+                        <url>${MY_SERVER_URL}/files/DiagPro_V2.apk</url>
+                    </x431PadSoft>
+
+                    <!-- 2. Firmware -->
+                    <x431PadSoft>
+                        <fileSize>393300</fileSize>
                         <lanId>EN</lanId>
                         <serverCurrentTime>2026-08-06</serverCurrentTime>
-                        <softUpdateTime>2025-03-08 00:00:00</softUpdateTime>
-                        <url>${MY_SERVER_URL}/files/DiagPro_V2.apk</url>
+                        <softId>873</softId>
+                        <softName>Firmware</softName>
+                        <softPackageID>DOWNLOAD</softPackageID>
+                        <softUpdateTime>2023-03-27 00:00:00</softUpdateTime>
+                        <versionDetailId>343730</versionDetailId>
+                        <versionNo>V11.91</versionNo>
+                        <url>${MY_SERVER_URL}/files/Firmware_V11.91.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 3. VIN Recognition App -->
+                    <x431PadSoft>
+                        <fileSize>6166636</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>880</softId>
+                        <softName>VIN Recognition App</softName>
+                        <softPackageID>VIN_RECOGNITION_APP</softPackageID>
+                        <softUpdateTime>2024-05-02 00:00:00</softUpdateTime>
+                        <versionDetailId>354418</versionDetailId>
+                        <versionNo>V1.01.006</versionNo>
+                        <url>${MY_SERVER_URL}/files/VIN_App.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 4. DEMO (ماركة تجريبية) -->
+                    <x431PadSoft>
+                        <fileSize>1024567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2001</softId>
+                        <softName>DEMO</softName>
+                        <softPackageID>DEMO</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500001</versionDetailId>
+                        <versionNo>V15.00</versionNo>
+                        <url>${MY_SERVER_URL}/files/DEMO_V15.00.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 5. EOBD2 -->
+                    <x431PadSoft>
+                        <fileSize>2048567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2002</softId>
+                        <softName>EOBD2 Protocol</softName>
+                        <softPackageID>EOBD2</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500002</versionDetailId>
+                        <versionNo>V22.80</versionNo>
+                        <url>${MY_SERVER_URL}/files/EOBD2_V22.80.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 6. TOYOTA -->
+                    <x431PadSoft>
+                        <fileSize>5048567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2003</softId>
+                        <softName>TOYOTA / LEXUS</softName>
+                        <softPackageID>TOYOTA</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500003</versionDetailId>
+                        <versionNo>V50.10</versionNo>
+                        <url>${MY_SERVER_URL}/files/TOYOTA_V50.10.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 7. VOLKSWAGEN -->
+                    <x431PadSoft>
+                        <fileSize>4048567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2004</softId>
+                        <softName>VOLKSWAGEN</softName>
+                        <softPackageID>VOLKSWAGEN</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500004</versionDetailId>
+                        <versionNo>V28.50</versionNo>
+                        <url>${MY_SERVER_URL}/files/VW_V28.50.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 8. MERCEDES-BENZ -->
+                    <x431PadSoft>
+                        <fileSize>6048567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2005</softId>
+                        <softName>MERCEDES-BENZ</softName>
+                        <softPackageID>BENZ</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500005</versionDetailId>
+                        <versionNo>V49.90</versionNo>
+                        <url>${MY_SERVER_URL}/files/BENZ_V49.90.zip</url>
+                    </x431PadSoft>
+
+                    <!-- 9. BMW -->
+                    <x431PadSoft>
+                        <fileSize>5548567</fileSize>
+                        <lanId>EN</lanId>
+                        <serverCurrentTime>2026-08-06</serverCurrentTime>
+                        <softId>2006</softId>
+                        <softName>BMW / MINI</softName>
+                        <softPackageID>BMW</softPackageID>
+                        <softUpdateTime>2026-01-01 00:00:00</softUpdateTime>
+                        <versionDetailId>500006</versionDetailId>
+                        <versionNo>V50.00</versionNo>
+                        <url>${MY_SERVER_URL}/files/BMW_V50.00.zip</url>
                     </x431PadSoft>
                 </x431PadSoftList>
             </return>
@@ -81,99 +175,7 @@ app.all([
     return res.status(200).send(xmlResponse);
 });
 
-// ----------------------------------------------------
-// 3. مسار الماركات المشتركة (queryPDTDiagSoftSubPack)
-// ----------------------------------------------------
-app.all([
-    '/api/v2/diagsoftservice',
-    '/diagsoftservice',
-    '/api/v2/x431paddiagsoftservice',
-    '/x431paddiagsoftservice'
-], (req, res) => {
-    res.setHeader('Content-Type', 'text/xml; charset=utf-8');
-    console.log("✅ [DIAGSOFT]: Responding with correct SOAP Envelope for queryPDTDiagSoftSubPack");
-
-    const xmlResponse = `<?xml version="1.0" encoding="UTF-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://diagzone.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-    <SOAP-ENV:Body>
-        <ns1:queryPDTDiagSoftSubPackResponse SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-            <return xsi:type="ns1:ResponseModel">
-                <code xsi:type="xsd:int">0</code>
-                <message xsi:type="xsd:string">success</message>
-                <diagSoftList href="#id1"/>
-            </return>
-        </ns1:queryPDTDiagSoftSubPackResponse>
-        <SOAP-ENC:Array id="id1" xsi:type="SOAP-ENC:Array" SOAP-ENC:arrayType="ns1:DiagSoft[6]">
-            <item href="#id2"/>
-            <item href="#id3"/>
-            <item href="#id4"/>
-            <item href="#id5"/>
-            <item href="#id6"/>
-            <item href="#id7"/>
-        </SOAP-ENC:Array>
-        <ns1:DiagSoft id="id2" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2001</softId>
-            <softPackageId xsi:type="xsd:string">DEMO</softPackageId>
-            <softPackageName xsi:type="xsd:string">DEMO</softPackageName>
-            <version xsi:type="xsd:string">V15.00</version>
-            <versionDetailId xsi:type="xsd:int">500001</versionDetailId>
-            <fileSize xsi:type="xsd:long">1024567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/DEMO_V15.00.zip</url>
-        </ns1:DiagSoft>
-        <ns1:DiagSoft id="id3" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2002</softId>
-            <softPackageId xsi:type="xsd:string">EOBD2</softPackageId>
-            <softPackageName xsi:type="xsd:string">EOBD2 Protocol</softPackageName>
-            <version xsi:type="xsd:string">V22.80</version>
-            <versionDetailId xsi:type="xsd:int">500002</versionDetailId>
-            <fileSize xsi:type="xsd:long">2048567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/EOBD2_V22.80.zip</url>
-        </ns1:DiagSoft>
-        <ns1:DiagSoft id="id4" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2003</softId>
-            <softPackageId xsi:type="xsd:string">TOYOTA</softPackageId>
-            <softPackageName xsi:type="xsd:string">TOYOTA / LEXUS</softPackageName>
-            <version xsi:type="xsd:string">V50.10</version>
-            <versionDetailId xsi:type="xsd:int">500003</versionDetailId>
-            <fileSize xsi:type="xsd:long">5048567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/TOYOTA_V50.10.zip</url>
-        </ns1:DiagSoft>
-        <ns1:DiagSoft id="id5" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2004</softId>
-            <softPackageId xsi:type="xsd:string">VOLKSWAGEN</softPackageId>
-            <softPackageName xsi:type="xsd:string">VOLKSWAGEN</softPackageName>
-            <version xsi:type="xsd:string">V28.50</version>
-            <versionDetailId xsi:type="xsd:int">500004</versionDetailId>
-            <fileSize xsi:type="xsd:long">4048567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/VW_V28.50.zip</url>
-        </ns1:DiagSoft>
-        <ns1:DiagSoft id="id6" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2005</softId>
-            <softPackageId xsi:type="xsd:string">BENZ</softPackageId>
-            <softPackageName xsi:type="xsd:string">MERCEDES-BENZ</softPackageName>
-            <version xsi:type="xsd:string">V49.90</version>
-            <versionDetailId xsi:type="xsd:int">500005</versionDetailId>
-            <fileSize xsi:type="xsd:long">6048567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/BENZ_V49.90.zip</url>
-        </ns1:DiagSoft>
-        <ns1:DiagSoft id="id7" xsi:type="ns1:DiagSoft">
-            <softId xsi:type="xsd:int">2006</softId>
-            <softPackageId xsi:type="xsd:string">BMW</softPackageId>
-            <softPackageName xsi:type="xsd:string">BMW / MINI</softPackageName>
-            <version xsi:type="xsd:string">V50.00</version>
-            <versionDetailId xsi:type="xsd:int">500006</versionDetailId>
-            <fileSize xsi:type="xsd:long">5548567</fileSize>
-            <url xsi:type="xsd:string">${MY_SERVER_URL}/files/BMW_V50.00.zip</url>
-        </ns1:DiagSoft>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>`;
-
-    return res.status(200).send(xmlResponse);
-});
-
-// ----------------------------------------------------
-// 4. مسار الروابط الأساسية (/urls)
-// ----------------------------------------------------
+// مسار خريطة الروابط الأساسية (/urls)
 app.all(['/urls', '/api/v2/urls'], (req, res) => {
     res.json({
         "code": 0,
@@ -187,8 +189,9 @@ app.all(['/urls', '/api/v2/urls'], (req, res) => {
                 { "key": "productservice.*", "value": `${MY_SERVER_URL}/api/v2/product-service` },
                 { "key": "publicsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/publicsoftservice` },
                 { "key": "publicsoftservice.nt", "value": `${MY_SERVER_URL}/api/v2/publicsoftservice-nt` },
-                { "key": "x431paddiagsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/diagsoftservice` },
-                { "key": "diagsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/diagsoftservice` },
+                { "key": "x431padpublicsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/publicsoftservice` },
+                { "key": "x431paddiagsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/publicsoftservice` },
+                { "key": "diagsoftservice.*", "value": `${MY_SERVER_URL}/api/v2/publicsoftservice` },
                 { "key": "publicsoft.download", "value": `${MY_SERVER_URL}/api/v2/download` },
                 { "key": "downloaddiagsoftws.action", "value": `${MY_SERVER_URL}/api/v2/download` }
             ]
@@ -196,9 +199,7 @@ app.all(['/urls', '/api/v2/urls'], (req, res) => {
     });
 });
 
-// ----------------------------------------------------
-// 5. تسجيل الدخول (/login)
-// ----------------------------------------------------
+// مسار تسجيل الدخول (/login)
 app.all(['/login', '/api/v2/login'], (req, res) => {
     res.json({
         "code": 0,
@@ -211,9 +212,6 @@ app.all(['/login', '/api/v2/login'], (req, res) => {
     });
 });
 
-// ----------------------------------------------------
-// 6. Catch-All لأي مسارات أخرى
-// ----------------------------------------------------
 app.all('*', (req, res) => {
     res.json({
         "code": 0,
@@ -224,5 +222,5 @@ app.all('*', (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-    console.log(`🚀 Complete Diag Server Online on Port ${PORT}`);
+    console.log(`🚀 Perfect Diag Server Online on Port ${PORT}`);
 });
