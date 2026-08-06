573877e5ee77c85c83f5aba9cedc5e9be23eb27c
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-08-06 13:31:20 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,19 +1,20 @@
-const express = require('express');
+const Express = require('express');
 const cors = require('cors');
 const path = require('path');
 
-const app = express();
+const app = Express();
 
 // ----------------------------------------------------
 // 1. الإعدادات والوسائط (Middlewares)
 // ----------------------------------------------------
 app.use(cors());
-app.use(express.json());
-app.use(express.text({ type: ['text/xml', 'application/xml', 'text/plain'] }));
-app.use(express.urlencoded({ extended: true }));
+app.use(Express.json());
+// دعم استقبال جزيئات الـ SOAP كـ Text أو Buffer
+app.use(Express.text({ type: '*/*' })); 
+app.use(Express.urlencoded({ extended: true }));
 
 // مجلد الملفات العامة
-app.use('/files', express.static(path.join(__dirname, 'public/files')));
+app.use('/files', Express.static(path.join(__dirname, 'public/files')));
 
 const MY_SERVER_URL = process.env.SERVER_URL || "https://my-diag-server.onrender.com";
 
@@ -56,7 +57,7 @@ app.all('/login', (req, res) => {
 });
 
 // ----------------------------------------------------
-// 4. مسار البرامج العامة الشغال لديك (/api/v2/publicsoftservice-nt)
+// 4. مسار البرامج العامة (/api/v2/publicsoftservice-nt)
 // ----------------------------------------------------
 app.all(['/api/v2/publicsoftservice-nt', '/publicsoftservice-nt', '/publicsoftservice'], (req, res) => {
     res.setHeader('Content-Type', 'text/xml; charset=utf-8');
@@ -68,7 +69,7 @@ app.all(['/api/v2/publicsoftservice-nt', '/publicsoftservice-nt', '/publicsoftse
             <out>
                 <code>0</code>
                 <message>success</message>
-                <result>
+
                     <item>
                         <softPackageId>Firmware</softPackageId>
                         <softPackageName>Firmware</softPackageName>
@@ -81,7 +82,7 @@ app.all(['/api/v2/publicsoftservice-nt', '/publicsoftservice-nt', '/publicsoftse
                         <version>V2.00.033</version>
                         <url>${MY_SERVER_URL}/files/DiagPro_V2.apk</url>
                     </item>
-                </result>
+
             </out>
         </queryLatestVersionResponse>
     </soap:Body>
@@ -91,72 +92,69 @@ app.all(['/api/v2/publicsoftservice-nt', '/publicsoftservice-nt', '/publicsoftse
 });
 
 // ----------------------------------------------------
-// 5. مسار الماركات المعدل ذكياً (/api/v2/diagsoftservice)
+// 5. مسار الماركات المصلح بالكامل (/api/v2/diagsoftservice)
 // ----------------------------------------------------
 app.all(['/api/v2/diagsoftservice', '/diagsoftservice'], (req, res) => {
     res.setHeader('Content-Type', 'text/xml; charset=utf-8');
 
-    const requestBody = String(req.body || '');
+    const rawBody = typeof req.body === 'string' ? req.body : JSON.stringify(req.body || '');
 
-    // قائمة الماركات المراد إظهارها
-    const brandsList = `
-        <item>
-            <softPackageId>DEMO</softPackageId>
-            <softPackageName>DEMO</softPackageName>
-            <version>V15.00</version>
-            <url>${MY_SERVER_URL}/files/DEMO_V15.00.zip</url>
-        </item>
-        <item>
-            <softPackageId>EOBD2</softPackageId>
-            <softPackageName>EOBD2 Protocol</softPackageName>
-            <version>V22.80</version>
-            <url>${MY_SERVER_URL}/files/EOBD2_V22.80.zip</url>
-        </item>
-        <item>
-            <softPackageId>TOYOTA</softPackageId>
-            <softPackageName>TOYOTA / LEXUS</softPackageName>
-            <version>V50.10</version>
-            <url>${MY_SERVER_URL}/files/TOYOTA_V50.10.zip</url>
-        </item>
-        <item>
-            <softPackageId>VOLKSWAGEN</softPackageId>
-            <softPackageName>VW / AUDI</softPackageName>
-            <version>V28.50</version>
-            <url>${MY_SERVER_URL}/files/VW_V28.50.zip</url>
-        </item>
-        <item>
-            <softPackageId>BENZ</softPackageId>
-            <softPackageName>MERCEDES-BENZ</softPackageName>
-            <version>V49.90</version>
-            <url>${MY_SERVER_URL}/files/BENZ_V49.90.zip</url>
-        </item>
-        <item>
-            <softPackageId>BMW</softPackageId>
-            <softPackageName>BMW / MINI</softPackageName>
-            <version>V50.00</version>
-            <url>${MY_SERVER_URL}/files/BMW_V50.00.zip</url>
-        </item>`;
-
-    // التمييز حسب اسم الدالة المطلوبة في جسم الـ SOAP Request
-    let responseAction = "queryPDTDiagSoftSubPackResponse";
-    if (requestBody.includes("queryLatestDiagSoftsIncrCdn")) {
-        responseAction = "queryLatestDiagSoftsIncrCdnResponse";
-    }
-
-    const xmlResponse = `<?xml version="1.0" encoding="UTF-8"?>
-<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:ns1="https://diagzone.com" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/">
-    <SOAP-ENV:Body>
-        <ns1:${responseAction}>
-            <return>
+    // إرجاع رد مزدوج الدعم يستجيب للبنية الخاصة بـ Diagzone
+    const xmlResponse = `<?xml version="1.0" encoding="utf-8"?>
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema">
+    <soap:Body>
+        <queryLatestDiagSoftsResponse xmlns="https://diagzone.com">
+            <result>
                 <code>0</code>
                 <message>success</message>
-                <result>
-                    ${brandsList}
-                </result>
-            </return>
-        </ns1:${responseAction}>
-    </SOAP-ENV:Body>
-</SOAP-ENV:Envelope>`;
+                <diagsofts>
+                    <item>
+                        <softPackageId>DEMO</softPackageId>
+                        <softPackageName>DEMO</softPackageName>
+                        <version>V15.00</version>
+                        <sysId>1</sysId>
+                        <url>${MY_SERVER_URL}/files/DEMO_V15.00.zip</url>
+                    </item>
+                    <item>
+                        <softPackageId>EOBD2</softPackageId>
+                        <softPackageName>EOBD2 Protocol</softPackageName>
+                        <version>V22.80</version>
+                        <sysId>2</sysId>
+                        <url>${MY_SERVER_URL}/files/EOBD2_V22.80.zip</url>
+                    </item>
+                    <item>
+                        <softPackageId>TOYOTA</softPackageId>
+                        <softPackageName>TOYOTA / LEXUS</softPackageName>
+                        <version>V50.10</version>
+                        <sysId>3</sysId>
+                        <url>${MY_SERVER_URL}/files/TOYOTA_V50.10.zip</url>
+                    </item>
+                    <item>
+                        <softPackageId>VOLKSWAGEN</softPackageId>
+                        <softPackageName>VW / AUDI</softPackageName>
+                        <version>V28.50</version>
+                        <sysId>4</sysId>
+                        <url>${MY_SERVER_URL}/files/VW_V28.50.zip</url>
+                    </item>
+                    <item>
+                        <softPackageId>BENZ</softPackageId>
+                        <softPackageName>MERCEDES-BENZ</softPackageName>
+                        <version>V49.90</version>
+                        <sysId>5</sysId>
+                        <url>${MY_SERVER_URL}/files/BENZ_V49.90.zip</url>
+                    </item>
+                    <item>
+                        <softPackageId>BMW</softPackageId>
+                        <softPackageName>BMW / MINI</softPackageName>
+                        <version>V50.00</version>
+                        <sysId>6</sysId>
+                        <url>${MY_SERVER_URL}/files/BMW_V50.00.zip</url>
+                    </item>
+                </diagsofts>
+            </result>
+        </queryLatestDiagSoftsResponse>
+    </soap:Body>
+</soap:Envelope>`;
 
     res.status(200).send(xmlResponse);
 });
