2357c7097400a113fa43faaacaa6f45acc9aa1af
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-08-15 17:18:21 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-لتنثاتيتيت
+لت<?xml version="1.0" encoding="utf-8"?>
+<network-security-config>
+    <base-config cleartextTrafficPermitted="true">
+        <trust-anchors>
+            <!-- السماح بشهادات النظام -->
+            <certificates src="system" />
+            <!-- السماح بشهادات المستخدم (لمراقبة واعتراض الاتصالات) -->
+            <certificates src="user" />
+        </trust-anchors>
+    </base-config>
+</network-security-config>
