7e3c95c8ac821e9844cf2bf967c61102dac8370f
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-08-26 18:41:47 +0300

    Update fileMap with new and modified links

--- server.js
+++ server.js
@@ -208,11 +208,11 @@ app.all('/api/v2/diagsoftservice', (req, res) => {
 // 10. خريطة الروابط للتحميل المباشر من GitHub Releases (مكتملة بـ 70 نسخة وهمية)
 const fileMap = {
     "362272": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/EOBD2_2312_AR.ZIP",
-    "5565": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/EOBD2_2312_AR.ZIP",
+    "2855": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/EOBD2_2312_AR.ZIP",
     "365206": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/AUTOSEARCH_1115_AR.ZIP",
     "367837": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/BMS_DEMO_1555_AR.ZIP",
     "380901": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/DEMO_1568_AR.ZIP",
-    "1211": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/ECUAID_1211_EN.ZIP",
+    "366146": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/ECUAID_1211_EN.ZIP",
     "4001": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/FILE_DEMO_AR.ZIP",
     "4002": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/FILE_DEMO_AR.ZIP",
     "4003": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/FILE_DEMO_AR.ZIP",
