8c4c53daa051f21929cc53837d634b66f0301cb1
77665645abohashim-ai <77665645abohashim@gmail.com>
2026-09-01 13:10:31 +0300

    Update download link for DEMO_THINKDIAG1

--- server.js
+++ server.js
@@ -390,7 +390,7 @@ const fileMap = {
     "2855": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/EOBD2_2312_AR.ZIP",
     "365206": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/AUTOSEARCH_1115_AR.ZIP",
     "367837": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/BMS_DEMO_1555_AR.ZIP",
-    "380901": "https://downloadapp.mythinkcar.com/app_soft/DEMO/2/V10.66/Arabic/DEMO_THINKDIAG1_V10.66_AR.zip",
+    "380901": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/DEMO.zip",
     "366146": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/ECUAID_1576511_V10.55_EN.ZIP",
     "4001": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/TOYOTA_THINKDIAG_V10.62_EN.zip",
     "4002": "https://github.com/77665645abohashim-ai/my-diag-server/releases/download/v1/FILE_DEMO_AR.ZIP",
